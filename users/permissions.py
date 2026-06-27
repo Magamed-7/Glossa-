1a55f5b9364e3651db00348a788ab76845d51c44
Magamed-7 <teachermaga7@gmail.com>
2026-06-27 22:26:05 +0500

    create permissions for users

---
+++ users/permissions.py
@@ -0,0 +1,25 @@
+from rest_framework.permissions import BasePermission
+
+class IsDashboardUser(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user and
+            request.user.is_authenticated and
+            request.user.dashboard_role in ['superadmin', 'moderator', 'analyst']
+        )
+
+class IsSuperadmin(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user and
+            request.user.is_authenticated and
+            request.user.dashboard_role == 'superadmin'
+        )
+
+class IsModerator(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user and
+            request.user.is_authenticated and
+            request.user.dashboard_role in ['superadmin', 'moderator']
+        )
